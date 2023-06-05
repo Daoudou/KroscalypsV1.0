cbc4ae9693adec8f733074cd5aaa02c59ff56fee
Daoudou <98310378+Daoudou@users.noreply.github.com>
2023-06-05 08:20:41 +0200

    Main push

--- main.cpp
+++ main.cpp
@@ -2,28 +2,23 @@
 //
 
 #include <iostream>
+#include <stdexcept>
+#include <fstream>
 #include <string.h>
 #include <map>
 #include <windows.h>
+#include <vector>
 #include <mmsystem.h>
 #include <stdio.h>
+#include <strsafe.h>
 #include <cstdlib>
 #include <ctime>
 
+#pragma comment(lib,"User32.lib")
+#define YES 1
+#define NO 0
+
 using namespace std;
-
-// This enum serv us for the string values in the switch case
-/*static enum StringValue {
-    StringValue1,
-    StringValue2
-};
-
-// Map to associate the strings with the enum values
-static map<string, StringValue> s_mapStringValue;
-
-static char ch[_MAX_PATH];
-
-static void Initialize();*/
 
 void getTimeKroscalypse() {
 
@@ -33,21 +28,55 @@ void getTimeKroscalypse() {
 
 }
 
+string exec(const char* cmd){
+
+    char buffer[128];
+    string result = "";
+    FILE* pipe = _popen(cmd,"r");
+    if(!pipe) throw runtime_error("popen() failed!");
+    try{
+        while(fgets(buffer,sizeof buffer, pipe) !=NULL){
+            result+= buffer;
+        }
+    }
+    catch(exception e){
+        _pclose(pipe);
+        throw;
+    }
+
+    _pclose(pipe);
+    return result;
+}
+
 int main()
 {
     system("cls");
 
-    cout << "\t<############################ PERSONAL ASSISSTANT ##############################>" << endl << endl;
+    cout << "\t<############################ ASTOLFO ASSISSTANT ##############################>" << endl << endl;
 
     string password;
     string commandUser;
     string nameProg;
+    string system32 = "cd C:/Users/Daoudou";
+    string todo;
+    string deleteTodo;
+    vector<string> todolist;
+
+    ofstream fileTest;
+    fileTest.open("testGetCmd.txt");
+
+   WIN32_FIND_DATA ffd;
+   LARGE_INTEGER filesize;
+   TCHAR szDir[MAX_PATH];
+   size_t length_of_arg;
+   HANDLE hFind = INVALID_HANDLE_VALUE;
+   DWORD dwError=0;
 
     do
     {
         cout << "*******************************************************" << endl;
         cout << "| ENTER THE PASSWORD TO CONFIRM YOUR ARE MASTER" << endl;
-        string phrase = "ENTER THE PASSWORD TO CONFIRM YOUR ARE MASTER";
+        string phrase = "ENTER THE PASSWORD TO CONFIRM YOUR ARE MY MASTER";
         string command = "espeak \"" + phrase + "\"";
         const char* charCommand = command.c_str();
         system(charCommand);
@@ -94,14 +123,48 @@ int main()
                 }
                 else if(commandUser == "give me the time pls"){
                     getTimeKroscalypse();
-                }
-                else if(commandUser == "execute the music"){
-                    cout << "Okay sir, launch to the hell fire" << endl;
-                    string phrase = "Okay sir, launch to the hell fire";
+                }else if(commandUser == "find file for me"){
+                    string ext="";
+                    string answer ="";
+                    cout << "Okay sir, give me the name of the file" << endl;
+                    string phrase = "Okay sir, give me the name of the file";
                     string command = "espeak \"" + phrase + "\"";
                     const char* charCommand = command.c_str();
                     system(charCommand);
-                    ShellExecute(NULL,"open","test1.mp3",NULL,NULL,SW_NORMAL);
+
+                    getline(cin,nameProg);
+                    cout << "Do you have a specific extension, sir?" <<endl;
+                    getline(cin,answer);
+                    if(answer == "Yes"){
+                        cout << "Give me the extension" << endl;
+                        getline(cin,answer);
+                        ext = answer;
+                    }
+                    string str = exec((""+system32+" && dir ""\*."+ext+""+nameProg+"* /s/d").c_str());
+                    fileTest << str ;
+                    cout << str << endl;
+
+                }
+                else if(commandUser == "execute somme music"){
+                    cout << "Okay sir, tell me what music you want to play" << endl;
+                    string phrase = "Okay sir, tell me what music you want to play";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char* charCommand = command.c_str();
+                    system(charCommand);
+
+                    getline(cin,nameProg);
+                    ShellExecute(NULL,"open",(""+nameProg+".mp3").c_str(),NULL,NULL,SW_NORMAL);
+                }
+                else if(commandUser == "let's play somme video"){
+                    cout <<"Okay sir, tell me what video you want to play" << endl;
+                    string phrase = "Okay sir, tell me what video you want to play";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char* charCommand = command.c_str();
+                    system(charCommand);
+
+                    getline(cin,nameProg);
+                    ShellExecute(NULL,"open",(""+nameProg+".mp4").c_str(),NULL,NULL,SW_NORMAL);
+
                 }
                 else if(commandUser == "open notepad"){
                     cout << "Okay sir, opening of the IDE in process" << endl;
@@ -120,7 +183,121 @@ int main()
                     system(charCommand);
                     system("start http://www.google.com");
 
-                } else if(commandUser == "open som programm pls"){
+                }else if(commandUser == "search for me"){
+                    cout <<"Tell me what you want to search sir :" << endl;
+                    string phrase = "Tell me what you want to search sir";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char* charCommand = command.c_str();
+                    system(charCommand);
+
+                    getline(cin,nameProg);
+                    system(("start https://www.google.com/search?q="+nameProg+"").c_str());
+
+                }else if(commandUser == "open my todolist"){
+                    cout<< "Okay sir, give me somme second" << endl;
+                    string phrase = "Okay sir, give me somme second";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char* charCommand = command.c_str();
+                    system(charCommand);
+
+                    cout << "Enter what you want to do with your todolist ? ===>";
+                    getline(cin,commandUser);
+
+                    if(commandUser == "Add somme thing in my todolist"){
+                    cout <<"Okay sir :" << endl;
+                    string phrase = "Okay sir";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char* charCommand = command.c_str();
+                    system(charCommand);
+
+                    getline(cin,todo);
+
+                    todolist.push_back(todo.c_str());
+
+                    }else if(commandUser == "Export my todolist"){
+
+                            cout << "Enter a name of file" << endl;
+                            string fileExp = "";
+
+                            getline(cin,fileExp);
+
+                            string fileExt = "";
+                            int choice = 0;
+                            cout << "Choose a extension" << endl;
+
+                            cout << "1. Text file" << endl;
+                            cout << "2. PDF file" << endl;
+                            cout << "3. Excel file" << endl;
+                            cin >> choice;
+
+                            switch(choice){
+                            case 1:
+                                cout << "You choice a Text file" << endl;
+                                fileExt = "txt";
+                                break;
+                            case 2:
+                                cout << "You choice a PDF file" << endl;
+                                fileExt = "pdf";
+                                break;
+                            case 3:
+                                cout << "You choice a Excel file" << endl;
+                                fileExt = "xlsx";
+                                break;
+                            default:
+                                 cout << "Choice a number" <<endl;
+                                break;
+                            }
+
+                            ofstream todolistExp(""+fileExp+"."+fileExt+"",ios::app | ios::binary);
+
+                            for(int i = 0; i < todolist.size(); i++){
+                                todolistExp << todolist[i] << endl;
+                            }
+
+                            string phrase = "Todolist exported";
+                            cout << "Todolist exported" << endl;
+                            string command = "espeak \""+ phrase +"\"";
+                            const char* charCommand = command.c_str();
+                            system(charCommand);
+
+                    }else if (commandUser == "Show me my todolist"){
+
+                            if(todolist.size() == NULL){
+                                cout<<"You're todolist is empty sir"<< endl;
+                                string phrase = "You're todolist is empty sir";
+                                string command = "espeak \"" + phrase + "\"";
+                                const char* charCommand = command.c_str();
+                                system(charCommand);
+                            }else{
+                                cout<<"This is what you will do"<< endl;
+                                string phrase = "This is what you will do";
+                                string command = "espeak \"" + phrase + "\"";
+                                const char* charCommand = command.c_str();
+                                system(charCommand);
+                                for(unsigned int i = 0; i < todolist.size(); i++){
+                                    cout << endl;
+                                    cout << todolist[i] << endl;
+                                }
+                            }
+                    }else if(commandUser == "delete my todolist"){
+                        cout<<"Tell what you want to delete"<< endl;
+                        string phrase = "Tell what you want to delete";
+                        string command = "espeak \"" + phrase + "\"";
+                        const char* charCommand = command.c_str();
+                        system(charCommand);
+
+                        todolist.clear();
+
+                    }
+                }else if(commandUser == "List me all programm"){
+                        cout<<"Okay sir, list in progress"<< endl;
+                        string phrase = "Okay sir, list in progress";
+                        string command = "espeak \"" + phrase + "\"";
+                        const char* charCommand = command.c_str();
+                        system(charCommand);
+                        system((""+system32+" && tasklist").c_str());
+
+                }else if(commandUser == "open som programm pls"){
                     cout << "Okay sir, give me the name of the programm" << endl;
                     string phrase = "Okay sir, give me the name of the programm";
                     string command = "espeak \"" + phrase + "\"";
@@ -136,22 +313,9 @@ int main()
                         const char* charCommand = command.c_str();
                         system(charCommand);
                         CreateProcess(TEXT("C:\\Program Files (x86)\\Steam\\steam.exe"),NULL,NULL,NULL,FALSE,NULL,NULL,NULL,&startInfo,&processInfo);
-
                     }
 
-                }else if(commandUser == "search a certain file"){
-                    cout << "Okay sir, give me the name of the file" << endl;
-                    string phrase = "Okay sir, give me the name of the file";
-                    string command = "espeak \"" + phrase + "\"";
-                    const char* charCommand = command.c_str();
-                    system(charCommand);
-
-                    if(getline(cin,nameProg)!= NULL){
-                        systemt("dir "test1.mp3" /s");
-                    }
-
-                }
-                else if(commandUser == "end of the programm" || commandUser == "bye" || commandUser == "i give you a break"){
+                }else if(commandUser == "end of the programm" || commandUser == "bye" || commandUser == "i give you a break"){
 
                     cout << "Okay thanks sir. See you next time" << endl;
                     string phrase = "Okay thanks sir. See you next time";
@@ -189,17 +353,7 @@ int main()
 
     } while (1);
 
+    fileTest.close();
     return 0;
 
 }
-
-/*
-void Initialize()
-{
-    s_mapStringValue["First Value"] = StringValue1;
-    s_mapStringValue["Second Value"] = StringValue2;
-    cout << "s_mapStringValue containts"
-        << s_mapStringValue.size()
-    << " entries" << endl;
-
-}*/
